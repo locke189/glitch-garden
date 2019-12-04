ea3f8377e45f4f0ab938474714152b4bf3f78614
Juan Insuasti <juan.insuasti@gmail.com>
2019-12-04 09:07:08 -0500

    loading screen transitions

---
+++ Glitch Garden/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    [SerializeField] float delay = 3f;
+    [SerializeField] string sceneToLoad = "Main Menu";
+    [SerializeField] bool onStart = true;
+
+    void Start()
+    {
+        if (onStart) {
+            StartCoroutine(LoadSceneDelayed(sceneToLoad));
+        }
+    }
+
+    IEnumerator LoadSceneDelayed(string sceneLabel)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneLabel);
+    }
+}
