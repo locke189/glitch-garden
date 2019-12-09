d49b76d3f1b5c83c9c4866893702787b9c058164
Juan Insuasti <juan.insuasti@gmail.com>
2019-12-09 07:44:15 -0500

    attacker spawner

---
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+
+    [Range(0f, 10f)]
+    [SerializeField] float speed = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * speed);
+        Debug.Log("OK");
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/AttackerSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackerSpawner : MonoBehaviour
+{
+    [SerializeField] bool loop = true;
+    [SerializeField] GameObject enemy;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        while (loop) {
+            yield return new WaitForSeconds(Random.Range(1f, 5f));
+            Instantiate(enemy, transform.position, Quaternion.identity);
+        }
+    }
+
+
+}
